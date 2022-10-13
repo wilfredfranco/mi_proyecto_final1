f16d087e3dcd7c6c542235febc1e2b667f2a2451
Wilfred Franco <wilfredfranco87@gmail.com>
2022-10-13 15:16:55 -0300

    Ejemplos de la clase 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    suma = 4 + 3
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre":"Wilfred",
+        "apellido": suma,
+        })
+
+def index_dos(request, nombre, apellido):
+    
+    return render(request, "ejemplo/saludar.html", 
+    {
+        "nombre": nombre,
+        "apellido": apellido,
+        })
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5,6,7,8]}
+    )
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, index_dos, index_tres
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index), # ESTA ES LA NUEVA FUNCTION
+    path('saludar/<nombre>/<apellido>/', index_dos),
+    path('mostrar-notas/', index_tres),
 ]
+
