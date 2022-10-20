273d5c7e9237fb081afc8df38e69cc66b001fc30
Wilfred Franco <wilfredfranco87@gmail.com>
2022-10-19 23:25:00 -0300

    Se agrega la app blog y un template avanzado

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ejemplo.apps.EjemploConfig', # ESTA ES LA NUEVA APP !!
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
